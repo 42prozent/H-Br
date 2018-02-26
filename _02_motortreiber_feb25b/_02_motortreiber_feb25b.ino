e1ca5c2e0ca99882f7da625fe8d2d05afa9dd337
Unknown <freek.416@googlemail.com>
2018-02-26 08:15:23 +0100

    try to rebuild

--- _02_motortreiber_feb25b/_02_motortreiber_feb25b.ino
+++ _02_motortreiber_feb25b/_02_motortreiber_feb25b.ino
@@ -7,36 +7,68 @@
 //rightL
 //richtR
 
-int SVL = 0; // speed Value Left
-int DVL = 0; // direcktion Value left
-
+    int SVL; // speed Value Left
+    int DVL; // direcktion Value left
+    
 void setup() {
+
   pinMode (11, OUTPUT); // Motor Speed about the enable pin
   pinMode (12, OUTPUT); // Motor Left
   pinMode (13, OUTPUT); // Moter Reight
     
     Serial.begin(9600);
+
 }
 
 void loop() {
+delay (20);
+
+while (Serial.available() > 0){
+
+  SVL = Serial.parseInt() ;
+  DVL = Serial.parseInt() ;
+
+  if (Serial.read() == '\n'){
+    
+    Serial.println (SLV, HEX);
+  }
+
+}
+
+ 
+  
+   // inputSpeedL = Serial.read();
+   
+    
+    
+  //  if (inputSpeedL > 0){
+     //   engineOne_LF ();
+        delay (900);
+   // }
+    
+ 
 
 
-
-engineOne_LF ();
 
 digitalWrite (leftForward,  LOW); digitalWrite (leftBackward, LOW);
 
-delay (900);
 }
 
 
 int engineOne_LF (){   // engine_One_Left_Forward
   
-analogWrite  (leftSpeed, 80);
+analogWrite  (leftSpeed, SVL);
 digitalWrite (leftForward,  HIGH);
 digitalWrite (leftBackward, LOW);
+Serial.println (SVL, DEC);
 return;
+}
 
 
+
+
+
+int serialRead (){
+  
 }
 
